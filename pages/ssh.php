38a396cde8c1853ad66de8355fd23aaf89042c46
Hackrylix <hackrylix@gmail.com>
2012-10-16 14:28:19 +0200

    code cleanup 1

--- pages/ssh.php
+++ pages/ssh.php
@@ -1,103 +1,139 @@
 <?php
-
-if(isset($_GET[goback])){
-echo '<script type="text/javascript">
+if (isset($_GET[goback]))
+{
+    echo '<script type="text/javascript">
         window.location = "index.php"
         </script>';
 }
 
 
-$filename =$_POST['filename'];
+$filename = $_POST['filename'];
 $newdata = $_POST['newdata'];
 
-if ($newdata != "") { $newdata = ereg_replace(13,  "", $newdata);
- $fw = fopen($filename, 'w') or die('Could not open file! Empty?');
- $fb = fwrite($fw,stripslashes($newdata)) or die('Could not write to file');
- fclose($fw);
- $message = $strings["ssh-fileUpdate"]." " . $filename . "<br /><br />";
+if ($newdata != "")
+{
+    $newdata = ereg_replace(13, "", $newdata);
+    $fw = fopen($filename, 'w') or die('Could not open file! Empty?');
+    $fb = fwrite($fw, stripslashes($newdata)) or die('Could not write to file');
+    fclose($fw);
+    $message = $strings["ssh-fileUpdate"] . " " . $filename . "<br /><br />";
 }
 
-if (isset($_GET[deletekey])) {
-        exec("rm -rf /etc/dropbear/id_rsa");
-        $message = "<pre>".$strings["ssh-deleteKey-message"]." /etc/dropbear/id_rsa</pre>";
-        }
-
-if (isset($_GET[generatekey])) {
-        exec("rm -rf /etc/dropbear/id_rsa");
-        exec("dropbearkey -t rsa -f /etc/dropbear/id_rsa");
-        $message = "<pre>".$strings["ssh-generateKey-message"]." /etc/dropbear/id_rsa</pre>";
-        }
-
-if (isset($_GET[connect])) {
-        if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "") {
-                exec("echo /pineapple/ssh/ssh-connect.sh | at now");
-                sleep(2);
-        } else {
-        }
+if (isset($_GET[deletekey]))
+{
+    exec("rm -rf /etc/dropbear/id_rsa");
+    $message = "<pre>" . $strings["ssh-deleteKey-message"] . " /etc/dropbear/id_rsa</pre>";
 }
 
-if (isset($_GET[disconnect])) {
-        if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "") {
-        } else {
-                exec("kill `ps aux | grep -v -e ssh.php | awk '/[s]sh/{print $1}'`");
-                sleep(2);
-        }
+if (isset($_GET[generatekey]))
+{
+    exec("rm -rf /etc/dropbear/id_rsa");
+    exec("dropbearkey -t rsa -f /etc/dropbear/id_rsa");
+    $message = "<pre>" . $strings["ssh-generateKey-message"] . " /etc/dropbear/id_rsa</pre>";
 }
 
-if (isset($_GET[enablekeepalive])) {
-        if (exec("grep ssh-keepalive.sh /etc/crontabs/root") == "") {
-                exec("echo '*/5 * * * * /pineapple/ssh/ssh-keepalive.sh' >> /etc/crontabs/root");
-		exec("/etc/init.d/crond restart");
-                $message = "<pre>".$strings["ssh-keepAlive-enabled"]." <a href='jobs.php'><b>".$strings["ssh-jobs"]."</b></a>.</pre>";
-        } else {
-        }
+if (isset($_GET[connect]))
+{
+    if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "")
+    {
+        exec("echo /pineapple/ssh/ssh-connect.sh | at now");
+        sleep(2);
+    }
+    else
+    {
+        
+    }
 }
 
-if (isset($_GET[disablekeepalive])) {
-        exec("sed -i '/ssh-keepalive.sh/d' /etc/crontabs/root");
+if (isset($_GET[disconnect]))
+{
+    if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "")
+    {
+        
+    }
+    else
+    {
+        exec("kill `ps aux | grep -v -e ssh.php | awk '/[s]sh/{print $1}'`");
+        sleep(2);
+    }
+}
+
+if (isset($_GET[enablekeepalive]))
+{
+    if (exec("grep ssh-keepalive.sh /etc/crontabs/root") == "")
+    {
+        exec("echo '*/5 * * * * /pineapple/ssh/ssh-keepalive.sh' >> /etc/crontabs/root");
         exec("/etc/init.d/crond restart");
+        $message = "<pre>" . $strings["ssh-keepAlive-enabled"] . " <a href='jobs.php'><b>" . $strings["ssh-jobs"] . "</b></a>.</pre>";
+    }
+    else
+    {
+        
+    }
+}
+
+if (isset($_GET[disablekeepalive]))
+{
+    exec("sed -i '/ssh-keepalive.sh/d' /etc/crontabs/root");
+    exec("/etc/init.d/crond restart");
 }
 
 $sshonboot = (exec("grep ssh-connect.sh /etc/rc.local"));
 
-if (isset($_GET[enable])) {
-	if (exec("grep ssh-connect.sh /etc/rc.local") == "") {
-		exec("sed -i '/exit 0/d' /etc/rc.local");
-		exec("echo /pineapple/ssh/ssh-connect.sh >> /etc/rc.local");
-		exec("echo exit 0 >> /etc/rc.local");
-		$sshonboot = "true";
-	} else {
-	}
+if (isset($_GET[enable]))
+{
+    if (exec("grep ssh-connect.sh /etc/rc.local") == "")
+    {
+        exec("sed -i '/exit 0/d' /etc/rc.local");
+        exec("echo /pineapple/ssh/ssh-connect.sh >> /etc/rc.local");
+        exec("echo exit 0 >> /etc/rc.local");
+        $sshonboot = "true";
+    }
+    else
+    {
+        
+    }
 }
 
-if (isset($_GET[disable])) {
-	exec("sed -i '/ssh-connect.sh/d' /etc/rc.local");
-	$sshonboot = "";
+if (isset($_GET[disable]))
+{
+    exec("sed -i '/ssh-connect.sh/d' /etc/rc.local");
+    $sshonboot = "";
 }
-
-
 ?>
 
 
 <div class=sidePanelLeft style="min-width:400px;">
 
-<div class=sidePanelTitle><?=$strings["ssh-options"]?></div>
-<div class=sidePanelContent>
-<?php
-if ($sshonboot != ""){
-echo $strings["ssh-boot"]." <font color=\"lime\"><b>".$strings["ssh-enabled"]."</b></font>.&nbsp; | <a href=\"index.php?ssh&disable&disablekeepalive\"><b>".$strings["ssh-disable"]."</b></a><br />";
-} else { echo $strings["ssh-boot"]." <font color=\"red\"><b>".$strings["ssh-disabled"]."</b></font>. | <a href=\"index.php?ssh&enable\"><b>".$strings["ssh-enable"]."</b></a><br />"; }
-if (exec("grep ssh-keepalive.sh /etc/crontabs/root") == "") {
-echo $strings["ssh-persist"]." <font color='red'><b>".$strings["ssh-disabled"]."</b></font>. | <a href='index.php?ssh&enablekeepalive&enable'><b>".$strings["ssh-enable"]."</b></a><br />";
-} else { echo $strings["ssh-persist"]." <font color='lime'><b>".$strings["ssh-enabled"]."</b></font>.&nbsp; | <a href='index.php?ssh&disablekeepalive'><b>".$strings["ssh-disable"]."</b></a><br />"; }
-if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "") {
-         echo $strings["ssh-session"]." <font color=\"red\"><b>".$strings["ssh-disconnected"]."</b></font> | <a href=\"index.php?ssh&connect\"><b>".$strings["ssh-connect"]."</b></a><br /><br />";
-} else {
-        echo $strings["ssh-session"]." <font color=\"lime\"><b>".$strings["ssh-connected"]."</b></font>. &nbsp; | <a href=\"index.php?ssh&disconnect\"><b>".$strings["ssh-disconnect"]."</b></a><br /><br />";
-}
-
-?>
-</div>
+    <div class=sidePanelTitle><?= $strings["ssh-options"] ?></div>
+    <div class=sidePanelContent>
+    <?php
+    if ($sshonboot != "")
+    {
+        echo $strings["ssh-boot"] . " <font color=\"lime\"><b>" . $strings["ssh-enabled"] . "</b></font>.&nbsp; | <a href=\"index.php?ssh&disable&disablekeepalive\"><b>" . $strings["ssh-disable"] . "</b></a><br />";
+    }
+    else
+    {
+        echo $strings["ssh-boot"] . " <font color=\"red\"><b>" . $strings["ssh-disabled"] . "</b></font>. | <a href=\"index.php?ssh&enable\"><b>" . $strings["ssh-enable"] . "</b></a><br />";
+    }
+    if (exec("grep ssh-keepalive.sh /etc/crontabs/root") == "")
+    {
+        echo $strings["ssh-persist"] . " <font color='red'><b>" . $strings["ssh-disabled"] . "</b></font>. | <a href='index.php?ssh&enablekeepalive&enable'><b>" . $strings["ssh-enable"] . "</b></a><br />";
+    }
+    else
+    {
+        echo $strings["ssh-persist"] . " <font color='lime'><b>" . $strings["ssh-enabled"] . "</b></font>.&nbsp; | <a href='index.php?ssh&disablekeepalive'><b>" . $strings["ssh-disable"] . "</b></a><br />";
+    }
+    if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "")
+    {
+        echo $strings["ssh-session"] . " <font color=\"red\"><b>" . $strings["ssh-disconnected"] . "</b></font> | <a href=\"index.php?ssh&connect\"><b>" . $strings["ssh-connect"] . "</b></a><br /><br />";
+    }
+    else
+    {
+        echo $strings["ssh-session"] . " <font color=\"lime\"><b>" . $strings["ssh-connected"] . "</b></font>. &nbsp; | <a href=\"index.php?ssh&disconnect\"><b>" . $strings["ssh-disconnect"] . "</b></a><br /><br />";
+    }
+    ?>
+    </div>
 
 </div>
 <br />
@@ -107,56 +143,57 @@ if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "") {
 <br />
 <br />
 <div class=content>
-<?php echo "<font color=lime>".$message."</font><br />" ?>
-<div class=contentTitle><?=$strings["ssh-connectCommand"]?></div>
-<div class=contentContent>
-<?php
-$filename = "/pineapple/ssh/ssh-connect.sh";
-  $fh = fopen($filename, "r") or die("Could not open file! Empty?");
-  $data = fread($fh, filesize($filename)) or die("Could not read file!");
-  fclose($fh);
- echo "<form action='index.php?ssh' method= 'post' ><input type='hidden' name='filename' value='ssh/ssh-connect.sh'>
-<input type='text' name='newdata' style='width:100%' value='$data' /><center><input type='submit' value='".$strings["ssh-connectCommand-button"]."'></form>";
-?>
-</div><br /><br />
+        <?php echo "<font color=lime>" . $message . "</font><br />" ?>
+    <div class=contentTitle><?= $strings["ssh-connectCommand"] ?></div>
+    <div class=contentContent>
+        <?php
+        $filename = "/pineapple/ssh/ssh-connect.sh";
+        $fh = fopen($filename, "r") or die("Could not open file! Empty?");
+        $data = fread($fh, filesize($filename)) or die("Could not read file!");
+        fclose($fh);
+        echo "<form action='index.php?ssh' method= 'post' ><input type='hidden' name='filename' value='ssh/ssh-connect.sh'>
+<input type='text' name='newdata' style='width:100%' value='$data' /><center><input type='submit' value='" . $strings["ssh-connectCommand-button"] . "'></form>";
+        ?>
+    </div><br /><br />
 
-<div class=contentTitle><?=$strings["ssh-publicKey"]?></div>
-<div class=contentContent>
-<?php
-echo "<small>".$strings["ssh-publicKey-note"]."</small><br /><br />";
-        echo "<textarea name='newdata' rows='8' style='min-width:100%; background-color:black; color:white; border-style:dashed;'>";
-        $cmd="dropbearkey -f /etc/dropbear/id_rsa -y";
-        exec ($cmd, $output);
-        foreach($output as $outputline) {
-        echo ("$outputline\n");}
-        echo "</textarea>";
-?>
-<br /><br /><?=$strings["ssh-publicKey-noKey"]?> <a href="index.php?ssh&generatekey"><b><?=$strings["ssh-publicKey-generate"]?></b></a> | <a href="index.php?ssh&deletekey"><b><?=$strings["ssh-publicKey-delete"]?></b></a> <?=$strings["ssh-publicKey-delete-note"]?></a><br />
-</div><br /><br />
+    <div class=contentTitle><?= $strings["ssh-publicKey"] ?></div>
+    <div class=contentContent>
+            <?php
+            echo "<small>" . $strings["ssh-publicKey-note"] . "</small><br /><br />";
+            echo "<textarea name='newdata' rows='8' style='min-width:100%; background-color:black; color:white; border-style:dashed;'>";
+            $cmd = "dropbearkey -f /etc/dropbear/id_rsa -y";
+            exec($cmd, $output);
+            foreach ($output as $outputline)
+            {
+                echo ("$outputline\n");
+            }
+            echo "</textarea>";
+            ?>
+        <br /><br /><?= $strings["ssh-publicKey-noKey"] ?> <a href="index.php?ssh&generatekey"><b><?= $strings["ssh-publicKey-generate"] ?></b></a> | <a href="index.php?ssh&deletekey"><b><?= $strings["ssh-publicKey-delete"] ?></b></a> <?= $strings["ssh-publicKey-delete-note"] ?></a><br />
+    </div><br /><br />
 
-<div class=contentTitle><?=$strings["ssh-knownHosts"]?></div>
-<div class=contentContent>
-<?php
-
-$filename = "/root/.ssh/known_hosts";
-$fh = fopen($filename, "r") or die("Could not open file!");
-$data = fread($fh, filesize($filename)) or die("Could not read file!");
-fclose($fh);
-echo "
+    <div class=contentTitle><?= $strings["ssh-knownHosts"] ?></div>
+    <div class=contentContent>
+            <?php
+            $filename = "/root/.ssh/known_hosts";
+            $fh = fopen($filename, "r") or die("Could not open file!");
+            $data = fread($fh, filesize($filename)) or die("Could not read file!");
+            fclose($fh);
+            echo "
 <form action='$_SERVER[php_self]' method= 'post' >
 <textarea name='newdata' rows='8' style='min-width:100%; background-color:black; color:white; border-style:dashed;'>$data</textarea>
 <input type='hidden' name='filename' value='/root/.ssh/known_hosts'>
-<center><input type='submit' value='".$strings["ssh-knownHosts-button"]."'>
+<center><input type='submit' value='" . $strings["ssh-knownHosts-button"] . "'>
 </form>";
-?>
+            ?>
 
-</div><br /><br />
+    </div><br /><br />
 </div>
 <div class=content style="min-width:950px">
-<div class=contentTitle><?=$strings["ssh-help"]?></div>
-<div class=contentContent>
-<pre>
-<?=$strings["ssh-help-content"]?>
+    <div class=contentTitle><?= $strings["ssh-help"] ?></div>
+    <div class=contentContent>
+        <pre>
+            <?= $strings["ssh-help-content"] ?>
 
 
 <b>SSH</b>
@@ -209,8 +246,8 @@ Environment variables are:
     AUTOSSH_DEBUG       - turn logging to maximum verbosity and log to
                           stderr
 
-</pre>
-</div><br /><br />
+        </pre>
+    </div><br /><br />
 
 
 </div>
--- pages/upgrade.php
+++ pages/upgrade.php
@@ -1,14 +1,15 @@
 <?php
+if (isset($_GET[finishOTA]))
+{
+    echo "<font color=lime>" . $strings["upgrade-ota-downloaded"] . "</font>";
+    $md5 = explode("|", trim(@file_get_contents("http://cloud.wifipineapple.com/index.php?downloads&currentVersion")));
+    if (exec("md5sum /tmp/upgrade.bin | grep -w " . $md5[1]) != "")
+    {
 
-if(isset($_GET[finishOTA])){
-        echo "<font color=lime>".$strings["upgrade-ota-downloaded"]."</font>";
-        $md5 = explode("|", trim(@file_get_contents("http://cloud.wifipineapple.com/index.php?downloads&currentVersion")));
-        if(exec("md5sum /tmp/upgrade.bin | grep -w ".$md5[1]) != ""){
+        $message = "<font color=lime>" . $strings["upgrade-working"] . "</font><br /><img src=\"includes/upgrade.gif\">";
+        exec("echo \"<?php echo 'working'; ?>\" > /pineapple/upgrade/upgradeStatus.php");
 
-                $message = "<font color=lime>".$strings["upgrade-working"]."</font><br /><img src=\"includes/upgrade.gif\">";
-                exec("echo \"<?php echo 'working'; ?>\" > /pineapple/upgrade/upgradeStatus.php");
-
-                echo "
+        echo "
                                 <script type='text/javascript' src='includes/jquery.min.js'></script>
                                 <script type='text/javascript'>
 
@@ -35,16 +36,20 @@ if(isset($_GET[finishOTA])){
 
                                 }
                                 </script>";
-	}else echo "<font color=red>".$strings["upgrade-ota-md5"]."</font>";
+    }
+    else
+        echo "<font color=red>" . $strings["upgrade-ota-md5"] . "</font>";
 }
 
-if(isset($_GET[doOTA])){
+if (isset($_GET[doOTA]))
+{
 
-	$connection = @file_get_contents("http://cloud.wifipineapple.com/ip.php");
-        if(trim($connection) != ""){
-        echo "<font color=lime>".$strings["upgrade-ota-downloading"]."</font><br /><br />";
-                        exec("echo \"<?php echo 'working'; ?>\" > /pineapple/upgrade/otaStatus.php");
-                        echo "
+    $connection = @file_get_contents("http://cloud.wifipineapple.com/ip.php");
+    if (trim($connection) != "")
+    {
+        echo "<font color=lime>" . $strings["upgrade-ota-downloading"] . "</font><br /><br />";
+        exec("echo \"<?php echo 'working'; ?>\" > /pineapple/upgrade/otaStatus.php");
+        echo "
                                 <script  type='text/javascript' src='includes/jquery.min.js'></script>
                                 <script type='text/javascript'>
 
@@ -72,51 +77,80 @@ if(isset($_GET[doOTA])){
                                 }
 
                                 </script>";
-	}else echo "<font color=red>".$strings["upgrade-ota-connectionError"]."</font>";
-
+    }
+    else
+        echo "<font color=red>" . $strings["upgrade-ota-connectionError"] . "</font>";
 }
 
-if(isset($_GET[checkUpgrade])){
-$remoteFile = explode("|", trim(@file_get_contents("http://cloud.wifipineapple.com/index.php?downloads&currentVersion")));
-$remoteMD5 = $remoteFile[1];
-$remoteVersion = explode(".", $remoteFile[0]);
-$localVersion = explode(".", file_get_contents("includes/fwversion"));
+if (isset($_GET[checkUpgrade]))
+{
+    $remoteFile = explode("|", trim(@file_get_contents("http://cloud.wifipineapple.com/index.php?downloads&currentVersion")));
+    $remoteMD5 = $remoteFile[1];
+    $remoteVersion = explode(".", $remoteFile[0]);
+    $localVersion = explode(".", file_get_contents("includes/fwversion"));
 
-if(trim($remoteFile[0]) == ""){
-echo "<font color=red>".$strings["upgrade-connectError"]."</font><br /><br />";
-}else{
-if($remoteVersion[0] > $localVersion[0]){
-	$upgradeMessage = $strings["upgrade-found"]." ($remoteVersion[0].$remoteVersion[1].$remoteVersion[2]) | <a href=\"?upgrade&doOTA\">".$strings["upgrade-doUpgrade"]."</a>";
-}else if($remoteVersion[0] == $localVersion[0]){
-        if($remoteVersion[1] > $localVersion[1]){
-		$upgradeMessage = $strings["upgrade-found"]." ($remoteVersion[0].$remoteVersion[1].$remoteVersion[2]) | <a href=\"?upgrade&doOTA\">".$strings["upgrade-doUpgrade"]."</a>";
-        }else if($remoteVersion[1] == $localVersion[1]){
-                if($remoteVersion[2] > $localVersion[2]){
-			$upgradeMessage = $strings["upgrade-found"]." ($remoteVersion[0].$remoteVersion[1].$remoteVersion[2]) | <a href=\"?upgrade&doOTA\">".$strings["upgrade-doUpgrade"]."</a>";
-                }else $upgradeMessage = $strings["upgrade-notFound"]." <a href=\"?upgrade&doOTA\">".$strings["upgrade-reflash"]." $remoteFile[0]?</a>";
-        }else $upgradeMessage = $strings["upgrade-notFound"]." <a href=\"?upgrade&doOTA\">".$strings["upgrade-reflash"]." $remoteFile[0]?</a>";
-}else $upgradeMessage = $strings["upgrade-notFound"]." <a href=\"?upgrade&doOTA\">".$strings["upgrade-reflash"]." $remoteFile[0]?</a>";
-}}
-
-if(isset($_GET[done])){
-	$message = "<font color=lime>".$strings["upgrade-complete"]."</font>";
+    if (trim($remoteFile[0]) == "")
+    {
+        echo "<font color=red>" . $strings["upgrade-connectError"] . "</font><br /><br />";
+    }
+    else
+    {
+        if ($remoteVersion[0] > $localVersion[0])
+        {
+            $upgradeMessage = $strings["upgrade-found"] . " ($remoteVersion[0].$remoteVersion[1].$remoteVersion[2]) | <a href=\"?upgrade&doOTA\">" . $strings["upgrade-doUpgrade"] . "</a>";
+        }
+        else if ($remoteVersion[0] == $localVersion[0])
+        {
+            if ($remoteVersion[1] > $localVersion[1])
+            {
+                $upgradeMessage = $strings["upgrade-found"] . " ($remoteVersion[0].$remoteVersion[1].$remoteVersion[2]) | <a href=\"?upgrade&doOTA\">" . $strings["upgrade-doUpgrade"] . "</a>";
+            }
+            else if ($remoteVersion[1] == $localVersion[1])
+            {
+                if ($remoteVersion[2] > $localVersion[2])
+                {
+                    $upgradeMessage = $strings["upgrade-found"] . " ($remoteVersion[0].$remoteVersion[1].$remoteVersion[2]) | <a href=\"?upgrade&doOTA\">" . $strings["upgrade-doUpgrade"] . "</a>";
+                }
+                else
+                    $upgradeMessage = $strings["upgrade-notFound"] . " <a href=\"?upgrade&doOTA\">" . $strings["upgrade-reflash"] . " $remoteFile[0]?</a>";
+            }
+            else
+                $upgradeMessage = $strings["upgrade-notFound"] . " <a href=\"?upgrade&doOTA\">" . $strings["upgrade-reflash"] . " $remoteFile[0]?</a>";
+        }
+        else
+            $upgradeMessage = $strings["upgrade-notFound"] . " <a href=\"?upgrade&doOTA\">" . $strings["upgrade-reflash"] . " $remoteFile[0]?</a>";
+    }
 }
 
-if(isset($_POST[doUpgrade])){
-	if($_FILES[upgrade][error] > 0){
-        	$error = $_FILES[upgrade][error];
-	        $message = $strings["upgrade-fileError"]." Error $error";
-	}else if(isset($_FILES[upgrade]) && !checkname()){
-	        $message = $strings["upgrade-nameError"];
-	}else if(isset($_FILES[upgrade])){
-	        exec("rm /tmp/upgrade*.bin");
-	        move_uploaded_file($_FILES[upgrade][tmp_name], "/tmp/upgrade.bin");
-	        if(exec("md5sum /tmp/upgrade.bin | grep -w ".trim($_POST[md5sum])) == ""){
-        	        $message = $strings["upgrade-md5Error"];
-	        }else{
-			$message = "<font color=lime>".$strings["upgrade-working"]."</font><br /><img src=\"includes/upgrade.gif\">";
-                        exec("echo \"<?php echo 'working'; ?>\" > /pineapple/upgrade/upgradeStatus.php");
-                        echo "
+if (isset($_GET[done]))
+{
+    $message = "<font color=lime>" . $strings["upgrade-complete"] . "</font>";
+}
+
+if (isset($_POST[doUpgrade]))
+{
+    if ($_FILES[upgrade][error] > 0)
+    {
+        $error = $_FILES[upgrade][error];
+        $message = $strings["upgrade-fileError"] . " Error $error";
+    }
+    else if (isset($_FILES[upgrade]) && !checkname())
+    {
+        $message = $strings["upgrade-nameError"];
+    }
+    else if (isset($_FILES[upgrade]))
+    {
+        exec("rm /tmp/upgrade*.bin");
+        move_uploaded_file($_FILES[upgrade][tmp_name], "/tmp/upgrade.bin");
+        if (exec("md5sum /tmp/upgrade.bin | grep -w " . trim($_POST[md5sum])) == "")
+        {
+            $message = $strings["upgrade-md5Error"];
+        }
+        else
+        {
+            $message = "<font color=lime>" . $strings["upgrade-working"] . "</font><br /><img src=\"includes/upgrade.gif\">";
+            exec("echo \"<?php echo 'working'; ?>\" > /pineapple/upgrade/upgradeStatus.php");
+            echo "
                                 <script  type='text/javascript' src='includes/jquery.min.js'></script>
                                 <script type='text/javascript'>
 
@@ -145,60 +179,66 @@ if(isset($_POST[doUpgrade])){
 
 
 				</script>";
-
-		}
-	}
+        }
+    }
 }
 
-function checkname(){
-        $file = $_FILES[upgrade][name];
-        $length = strlen($file);
-        $end = $length - 4;
-        $starts = (strpos($file, "upgrade") === 0) ? TRUE : FALSE ;
-        $ends = (strpos($file, ".bin") === $end) ? TRUE : FALSE ;
-        $hasspaces = (strpos($file, " ") === FALSE) ? TRUE : FALSE;
-        return ($starts && $ends && $hasspaces);
+function checkname()
+{
+    $file = $_FILES[upgrade][name];
+    $length = strlen($file);
+    $end = $length - 4;
+    $starts = (strpos($file, "upgrade") === 0) ? TRUE : FALSE;
+    $ends = (strpos($file, ".bin") === $end) ? TRUE : FALSE;
+    $hasspaces = (strpos($file, " ") === FALSE) ? TRUE : FALSE;
+    return ($starts && $ends && $hasspaces);
 }
-
 ?>
 
 <div class=content>
-<?php if($message != "") echo "<font color=lime>".$message."<font><br /><br />"; ?>
-<div class=contentTitle><?=$strings["upgrade-check-title"]?></div>
-<div class=contentContent>
-<center><?=$strings["upgrade-firmware"]?> <?php include('includes/fwversion'); ?><br />
-<a href="index.php?upgrade&checkUpgrade"><?=$strings["upgrade-check-link"]?></a></center>
-<?php if($upgradeMessage != "") echo "<br /><br /><font color=lime>".$upgradeMessage."</font>"; ?>
-</div><br /><br />
+    <?php if ($message != "") echo "<font color=lime>" . $message . "<font><br /><br />"; ?>
+    <div class=contentTitle><?= $strings["upgrade-check-title"] ?></div>
+    <div class=contentContent>
+        <center><?= $strings["upgrade-firmware"] ?> <?php include('includes/fwversion'); ?><br />
+            <a href="index.php?upgrade&checkUpgrade"><?= $strings["upgrade-check-link"] ?></a></center>
+        <?php if ($upgradeMessage != "") echo "<br /><br /><font color=lime>" . $upgradeMessage . "</font>"; ?>
+    </div><br /><br />
 
-<div class=contentTitle><?=$strings["upgrade-doUpgrade-title"]?></div>
-<div class=contentContent><center>
-<form action="" method="post" enctype="multipart/form-data" onSubmit="return confirm('<?=$strings["upgrade-doUpgrade-confirm"]?>')"><table>
-<tr><td>Upgrade.bin:</td><td><input type="file" value="upgrade.bin" name="upgrade" id="upgrade" /></td></tr>
-<tr><td>MD5:</td><td><input type="text" name="md5sum"></td></tr></table>
-<input type="submit" name="doUpgrade" value="<?=$strings["upgrade-doUpgrade-button"]?>">
-</form>
-</div><br /><br />
+    <div class=contentTitle><?= $strings["upgrade-doUpgrade-title"] ?></div>
+    <div class=contentContent>
+        <center>
+            <form action="" method="post" enctype="multipart/form-data" onSubmit="return confirm('<?= $strings["upgrade-doUpgrade-confirm"] ?>')">
+                <table>
+                    <tr><td>Upgrade.bin:</td><td><input type="file" value="upgrade.bin" name="upgrade" id="upgrade" /></td></tr>
+                    <tr><td>MD5:</td><td><input type="text" name="md5sum"></td></tr>
+                </table>
+                <input type="submit" name="doUpgrade" value="<?= $strings["upgrade-doUpgrade-button"] ?>">
+            </form>
+        </center>
+    </div><br /><br />
 
-<div class=contentTitle><?=$strings["upgrade-warning-title"]?></div>
-<div class=contentContent>
-<center>
-<pre><?=$strings["upgrade-warning"]?>
+    <div class=contentTitle><?= $strings["upgrade-warning-title"] ?></div>
+    <div class=contentContent>
+        <center>
+            <pre><?= $strings["upgrade-warning"] ?>
 <!-- Do not feed Pineapple after midnight -->
+            </pre>
+        </center>
+    </div><br /><br />
 
-</div><br /><br />
-
-<div class=contentTitle><?=$strings["upgrade-memory"]?></div>
-<div class=contentContent>
-<pre>
-<?php
-$cmd = "free";
-exec ($cmd, $output);
-foreach($output as $outputline) {
-echo ("$outputline\n");}
-?>
-</pre>
-</div><br /><br />
+    <div class=contentTitle><?= $strings["upgrade-memory"] ?></div>
+    <div class=contentContent>
+        <pre>
+            <?php
+            $cmd = "free";
+            exec($cmd, $output);
+            foreach ($output as $outputline)
+            {
+                echo ("$outputline\n");
+            }
+            ?>
+        </pre>
+    </div><br /><br />
 
 
 </div>
